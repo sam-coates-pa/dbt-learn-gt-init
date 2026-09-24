6a87c555810f02ef34d96ba59deae2fbaa52937e
Sam Coates <sam.coates@paconsulting.com>
2026-09-24 09:14:17 +0100

    Added source config

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -10,16 +10,24 @@ orders as (
 
 ),
 
+payments as (
+
+    select * from {{ ref('stg_stripe_payments') }}
+
+),
+
 customer_orders as (
 
     select
         customer_id,
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        min(o.order_date) as first_order_date,
+        max(o.order_date) as most_recent_order_date,
+        count(o.order_id) as number_of_orders,
+        sum(f.amount) as lifetime_value
 
-    from orders
+    from orders o
+    left join {{ ref('fct_orders') }} f using (customer_id)
 
     group by 1
 
@@ -34,7 +42,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value
 
     from customers
 
---
+++ models/staging/stripe/stg_stripe_payments.sql
@@ -0,0 +1,9 @@
+-- This is a dbt model that creates a view of payments from the raw.stripe.payment table.
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    amount / 100 as amount,
+    status
+
+from raw.stripe.payment
